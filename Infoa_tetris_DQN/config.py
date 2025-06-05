430ba7505189366d2569c80c554f6f50a0fb1c63
Altair <83115294+Altair1222@users.noreply.github.com>
2025-06-06 04:07:11 +0900

    Update config.py

--- Infoa_tetris_DQN/config.py
+++ Infoa_tetris_DQN/config.py
@@ -8,9 +8,9 @@ ENV_NAME = "TetrisEnv-v0"  # 実際には gymnasium.register で登録した ID
 # --- 学習ハイパーパラメータ ---
 SEED = 123
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-NUM_EPISODES = 2000
+NUM_EPISODES = 20000
 MAX_STEPS_PER_EPISODE = 10000  # 1エピソードあたりの最大ステップ数
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 0.99
 GAMMA = 0.99
 BATCH_SIZE = 32
 BUFFER_CAPACITY = 100000
